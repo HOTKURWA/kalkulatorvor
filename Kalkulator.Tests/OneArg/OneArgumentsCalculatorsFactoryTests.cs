bba03367fd2e81928aabdefe21ad6745090f6362
Fernand13 <55korol55@mail.ru>
2017-07-13 15:04:26 +0600

    Add Factory tests

---
+++ Kalkulator.Tests/OneArg/OneArgumentsCalculatorsFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Kalkulator.OneArg;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgument
+{
+    [TestFixture]
+    public class OneArgumentFactoryTest
+    {
+        [TestCase("cos", typeof(Cosinus))]
+        [TestCase("sin", typeof(Sinus))]
+        [TestCase("tan", typeof(Tangens))]
+        public void CalculateTest(string testName, Type expected)
+        {
+            var calculator = OneArgumentsCalculatorsFactory.CreateCalculator(testName);
+            Assert.AreEqual(expected, calculator.GetType());
+        }
+    }
+}
---
+++ Kalkulator.Tests/TwoArg/TwoArgumentsCalculatorsFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using Kalkulator.TwoArg;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    public class TwoArgumentFactoryTest
+    {
+        [TestCase("Sum", typeof(Addition))]
+        [TestCase("Mul", typeof(Multiply))]
+        [TestCase("Div", typeof(Division))]
+        public void CalculateTest(string testName, Type expected)
+        {
+            var calculator = TwoArgumentsCalculatorsFactory.CreateCalculator(testName);
+            Assert.AreEqual(expected, calculator.GetType());
+        }
+    }
